cff862581a61cc3da0ed45480ead36c6a2715d9b
szeigler <szeigler@vcu.edu>
2015-05-11 07:32:46 -0400

    RAMS-SPOT Training Updates

---
+++ osp/rams-spot_training/import subaward introduction/scripts/config_xml.js
@@ -0,0 +1,46 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description tsc:version="2.0.1" dc:date="2015-05-08 08:26:12 AM" dc:source="Camtasia Studio,8.5.1,enu" dc:title="import subaward introduction" tscDM:firstFrame="import_subaward_introduction_First_Frame.png" tscDM:originId="B24CE21B-762C-4CFF-958B-6B97073C0AD3" tscDM:project="import subaward introduction">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="185733"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="480" stDim:w="854"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="en-US">English</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="180200" xmpDM:duration="5530" tscDM:boundingPoly="324,296;537,296;537,416;324,416;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="http://www.research.vcu.edu/osp/rams-spot_training/import%20subaward%202/import%20subaward%202.html" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmp:label="2" xmpDM:startTime="180200" xmpDM:duration="5530" tscDM:boundingPoly="335,340;497,340;497,365;335,365;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="http://www.research.vcu.edu/osp/rams-spot_training/import%20subaward%202/import%20subaward%202.html" tscHS:newWindow="1"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="en-US"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="import subaward introduction.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="import_subaward_introduction_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="import_subaward_introduction_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
